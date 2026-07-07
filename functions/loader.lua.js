0979b9f53f322c3c3c15c27b683acfbd79f55002
Naellx <alifmodesad56@gmail.com>
2026-07-08 00:03:13 +0700

    Create loader.lua.js

---
+++ functions/loader.lua.js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  const ua = context.request.headers.get("User-Agent") || "";
+
+  if (
+    ua.includes("Mozilla") ||
+    ua.includes("Chrome") ||
+    ua.includes("Safari") ||
+    ua.includes("Firefox") ||
+    ua.includes("Edge")
+  ) {
+    return Response.redirect("https://mercuryproject.site", 302);
+  }
+
+  return fetch("https://raw.githubusercontent.com/Naellx/MercuryProject/main/loader.lua");
+}
