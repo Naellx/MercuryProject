f1db69ee28eec15a2d31d080ecfc5b785ae8ae4f
Naellx <alifmodesad56@gmail.com>
2026-07-08 00:14:19 +0700

    Update loader-source.lua

--- loader-source.lua
+++ loader-source.lua
@@ -1,23 +1,3 @@
-print("══════════════════════════════════════════════")
-print("        MERCURY PROJECT | PUBLIC LOADER")
-print("══════════════════════════════════════════════")
-print("🌐 Website : https://mercuryproject.site")
-print("══════════════════════════════════════════════")
+print("Mercury Project")
 
-local URL = "https://raw.githubusercontent.com/Naellx/MercuryProject/main/Main.lua"
-
-local Success, Response = pcall(function()
-    return game:HttpGet(URL)
-end)
-
-if not Success then
-    error("[Mercury Project] Failed to connect to server.")
-end
-
-local Chunk, Err = loadstring(Response)
-
-if not Chunk then
-    error("[Mercury Project] "..tostring(Err))
-end
-
-Chunk()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Naellx/MercuryProject/main/Main.lua"))()
