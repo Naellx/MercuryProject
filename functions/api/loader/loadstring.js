d3a68aaa1aca6f488717df9534954558f9289f5b
Naellx <alifmodesad56@gmail.com>
2026-07-24 15:58:25 +0700

    Delete functions/api directory

--- functions/api/loader/loadstring.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  const body = {
-    loadstring: 'loadstring(game:HttpGet("https://mercuryproject.site/loader.lua"))()'
-  };
-
-  return new Response(JSON.stringify(body), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
